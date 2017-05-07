272de13ce132d5ce046fd87a6170179501434f4e
omrobbie <putu.roby@yahoo.com>
2017-05-07 14:45:11 +0800

    mendapatkan value dari TextView

--- app/src/main/java/omrobbie/com/iakbeginner/MainActivity.java
+++ app/src/main/java/omrobbie/com/iakbeginner/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -14,11 +15,13 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_activity);
 
+        final TextView txtCenter = (TextView) findViewById(R.id.txtCenter);
         Button btn = (Button) findViewById(R.id.btn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Click detected!", Toast.LENGTH_SHORT).show();
+                String getText = txtCenter.getText().toString();
+                Toast.makeText(getApplicationContext(), getText, Toast.LENGTH_SHORT).show();
             }
         });
     }
