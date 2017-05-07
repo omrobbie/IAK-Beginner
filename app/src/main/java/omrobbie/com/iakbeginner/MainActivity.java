86903285e6c474016504dc1330e5f5325dea8a4a
omrobbie <putu.roby@yahoo.com>
2017-05-07 14:35:58 +0800

    mendeteksi click botton

--- app/src/main/java/omrobbie/com/iakbeginner/MainActivity.java
+++ app/src/main/java/omrobbie/com/iakbeginner/MainActivity.java
@@ -1,13 +1,25 @@
 package omrobbie.com.iakbeginner;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.happy_birthday);
+        setContentView(R.layout.layout_activity);
+
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Click detected!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
