c8f4dd5994f70b82929564d40151fa277cb571e8
omrobbie <putu.roby@yahoo.com>
2017-05-13 13:39:01 +0800

    tambahkan data otomatis dengan arraylist

--- app/src/main/java/omrobbie/com/iakbeginner/ActivityListView.java
+++ app/src/main/java/omrobbie/com/iakbeginner/ActivityListView.java
@@ -5,19 +5,26 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 import static android.R.attr.value;
 
 public class ActivityListView extends Activity{
 
-    String value[] = {"value a", "value b"};
-    
+    String value[] = {"value a", "value b", "value c", "value d", "value e"};
+    ArrayList<String> data = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_listview);
 
         ListView lvList = (ListView) findViewById(R.id.lvList);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, value);
+
+        for (int i=0; i<15; i++) {
+            data.add("Value " + i);
+        }
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, data);
         lvList.setAdapter(adapter);
     }
 }
