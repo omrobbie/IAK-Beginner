7dd237b70e857f3c4a815bfccba66d7807c1899f
omrobbie <putu.roby@yahoo.com>
2017-05-13 14:42:39 +0800

    deklarasikan variabel untuk adapter

--- app/src/main/java/omrobbie/com/iakbeginner/WeatherAdapter.java
+++ app/src/main/java/omrobbie/com/iakbeginner/WeatherAdapter.java
@@ -3,8 +3,18 @@ package omrobbie.com.iakbeginner;
 import android.content.Context;
 import android.widget.ArrayAdapter;
 
+import java.util.ArrayList;
+
 public class WeatherAdapter extends ArrayAdapter<Weather>{
-    public WeatherAdapter(Context context, int resource, Weather[] objects) {
+    Context context;
+    int layoutResourceId;
+    ArrayList<Weather> data = null;
+
+    public WeatherAdapter(Context context, int resource, ArrayList<Weather> objects) {
         super(context, resource, objects);
+
+        this.context = context;
+        this.layoutResourceId = resource;
+        this.data = objects;
     }
 }
