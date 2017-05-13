cccfeda4e0476d8f5f5e913115968277c42b39f1
omrobbie <putu.roby@yahoo.com>
2017-05-13 14:46:39 +0800

    bikin static class

--- app/src/main/java/omrobbie/com/iakbeginner/WeatherAdapter.java
+++ app/src/main/java/omrobbie/com/iakbeginner/WeatherAdapter.java
@@ -2,6 +2,8 @@ package omrobbie.com.iakbeginner;
 
 import android.content.Context;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,5 +18,10 @@ public class WeatherAdapter extends ArrayAdapter<Weather>{
         this.context = context;
         this.layoutResourceId = resource;
         this.data = objects;
+    }
+
+    static class WeatherHolder {
+        ImageView imgIcon;
+        TextView txtTitle;
     }
 }
