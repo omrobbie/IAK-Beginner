c705390aa751b88ef86c288fb3e471d9a8fa0fd9
omrobbie <putu.roby@yahoo.com>
2017-05-13 11:02:56 +0800

    tambahkan fungsi setOnClickListener

--- app/src/main/java/omrobbie/com/iakbeginner/ActivityCheckBox.java
+++ app/src/main/java/omrobbie/com/iakbeginner/ActivityCheckBox.java
@@ -2,7 +2,9 @@ package omrobbie.com.iakbeginner;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class ActivityCheckBox extends Activity {
 
@@ -11,8 +13,29 @@ public class ActivityCheckBox extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_checkbox);
 
-        CheckBox chkMusic = (CheckBox) findViewById(R.id.chkMusic);
-        CheckBox chkGame = (CheckBox) findViewById(R.id.chkGame);
-        CheckBox chkBlog = (CheckBox) findViewById(R.id.chkBlog);
+        final CheckBox chkMusic = (CheckBox) findViewById(R.id.chkMusic);
+        final CheckBox chkGame = (CheckBox) findViewById(R.id.chkGame);
+        final CheckBox chkBlog = (CheckBox) findViewById(R.id.chkBlog);
+
+        chkMusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), chkMusic.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        chkGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), chkGame.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        chkBlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), chkBlog.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
