cf21bc1d29cc2fb1dbec72113e6d39126af9f6a3
omrobbie <putu.roby@yahoo.com>
2017-05-13 14:57:47 +0800

    tempatkan holder di dalam getView

--- app/src/main/java/omrobbie/com/iakbeginner/WeatherAdapter.java
+++ app/src/main/java/omrobbie/com/iakbeginner/WeatherAdapter.java
@@ -1,7 +1,9 @@
 package omrobbie.com.iakbeginner;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -26,6 +28,19 @@ public class WeatherAdapter extends ArrayAdapter<Weather>{
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
+        View row = convertView;
+        WeatherHolder holder = null;
+
+        if (row == null) {
+            LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+            row = inflater.inflate(layoutResourceId, parent, false);
+        }
+
+        holder = new WeatherHolder();
+        holder.imgIcon = (ImageView) row.findViewById(R.id.imgIcon);
+        holder.txtTitle = (TextView) row.findViewById(R.id.txtTitle);
+
         return super.getView(position, convertView, parent);
     }
 
