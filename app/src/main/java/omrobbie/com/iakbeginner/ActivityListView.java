e731fe28fe3de48519650010ae6bbb0a5c79e489
omrobbie <putu.roby@yahoo.com>
2017-05-13 13:28:50 +0800

    memberikan nilai untuk listview

--- app/src/main/java/omrobbie/com/iakbeginner/ActivityListView.java
+++ app/src/main/java/omrobbie/com/iakbeginner/ActivityListView.java
@@ -2,15 +2,22 @@ package omrobbie.com.iakbeginner;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import static android.R.attr.value;
 
 public class ActivityListView extends Activity{
 
+    String value[] = {"value a", "value b"};
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_listview);
 
         ListView lvList = (ListView) findViewById(R.id.lvList);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, value);
+        lvList.setAdapter(adapter);
     }
 }
