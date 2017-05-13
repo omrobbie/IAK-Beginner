83e5f8788e592d3d5c9e61cf57f2a0295099c82e
omrobbie <putu.roby@yahoo.com>
2017-05-13 11:28:36 +0800

    tambahkan project radiobutton

---
+++ app/src/main/java/omrobbie/com/iakbeginner/ActivityRadioButton.java
@@ -0,0 +1,14 @@
+package omrobbie.com.iakbeginner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.RadioButton;
+
+public class ActivityRadioButton extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_radiobutton);
+    }
+}
