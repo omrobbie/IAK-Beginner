1abb795c26c3d360acf7044d5f139f12728712ad
omrobbie <putu.roby@yahoo.com>
2017-05-13 11:36:29 +0800

    tambahkan fungsi untuk membaca isi di dalam radiogroup

--- app/src/main/java/omrobbie/com/iakbeginner/ActivityRadioButton.java
+++ app/src/main/java/omrobbie/com/iakbeginner/ActivityRadioButton.java
@@ -2,7 +2,10 @@ package omrobbie.com.iakbeginner;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class ActivityRadioButton extends Activity{
 
@@ -10,5 +13,14 @@ public class ActivityRadioButton extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_radiobutton);
+
+        RadioGroup rbgVote = (RadioGroup) findViewById(R.id.rbgVote);
+        rbgVote.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton rb = (RadioButton) group.findViewById(checkedId);
+                Toast.makeText(getApplicationContext(), rb.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
