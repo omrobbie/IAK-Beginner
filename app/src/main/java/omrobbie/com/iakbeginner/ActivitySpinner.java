62f4608efad76a940ffea1e43705ecadb632ef12
omrobbie <putu.roby@yahoo.com>
2017-05-13 10:17:33 +0800

    bikin respon untuk tombol spinner (combobox)

--- app/src/main/java/omrobbie/com/iakbeginner/ActivitySpinner.java
+++ app/src/main/java/omrobbie/com/iakbeginner/ActivitySpinner.java
@@ -3,7 +3,10 @@ package omrobbie.com.iakbeginner;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,5 +23,23 @@ public class ActivitySpinner extends Activity {
         categories.add("Beginner");
         categories.add("Intermediate");
         categories.add("Advance");
+
+        final ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(dataAdapter);
+        spinner.setSelection(2);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = dataAdapter.getItem(position);
+                Toast.makeText(getApplicationContext(), "Selected : " + item, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
