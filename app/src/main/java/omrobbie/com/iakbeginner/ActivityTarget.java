e7c2e39dd511489a64c31310dc12e76dde42662b
omrobbie <putu.roby@yahoo.com>
2017-05-13 16:42:01 +0800

    final project

--- app/src/main/java/omrobbie/com/iakbeginner/ActivityTarget.java
+++ app/src/main/java/omrobbie/com/iakbeginner/ActivityTarget.java
@@ -2,7 +2,14 @@ package omrobbie.com.iakbeginner;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import static omrobbie.com.iakbeginner.R.id.txtJudul;
+import static omrobbie.com.iakbeginner.R.id.txtTitle;
 
 public class ActivityTarget extends Activity {
     @Override
@@ -10,7 +17,13 @@ public class ActivityTarget extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_target);
 
-        String getData = getIntent().getExtras().getString("data");
-        Toast.makeText(getApplicationContext(), getData, Toast.LENGTH_SHORT).show();
+        int getIcon = getIntent().getExtras().getInt("icon");
+        String getTitle = getIntent().getExtras().getString("title");
+
+        ImageView imgGambar = (ImageView) findViewById(R.id.imgGambar);
+        imgGambar.setImageResource(getIcon);
+
+        TextView txtJudul = (TextView) findViewById(R.id.txtJudul);
+        txtJudul.setText(getTitle);
     }
 }
--- app/src/main/java/omrobbie/com/iakbeginner/ActivityWeather.java
+++ app/src/main/java/omrobbie/com/iakbeginner/ActivityWeather.java
@@ -1,6 +1,7 @@
 package omrobbie.com.iakbeginner;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,6 +12,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 import static omrobbie.com.iakbeginner.R.id.lvList;
+import static omrobbie.com.iakbeginner.R.id.txtNama;
 
 public class ActivityWeather extends Activity {
 
@@ -25,10 +27,42 @@ public class ActivityWeather extends Activity {
 
         final ArrayList<Weather> data = new ArrayList<>();
 
-        for (i = 1; i <= 3; i++) {
-            Weather weather = new Weather(R.drawable.android_kejar, "Antroid Kejar " + i);
-            data.add(weather);
-        }
+//        for (i = 1; i <= 3; i++) {
+//            Weather weather = new Weather(R.drawable.android_kejar, "Antroid Kejar " + i);
+//            data.add(weather);
+//        }
+
+        // sementara, masukkan data secara manual
+        Weather jeruk = new Weather(R.drawable.jeruk, "jeruk");
+        data.add(jeruk);
+
+        Weather anggur = new Weather(R.drawable.anggur, "anggur");
+        data.add(anggur);
+
+        Weather kiwi = new Weather(R.drawable.kiwi, "kiwi");
+        data.add(kiwi);
+
+        Weather mangga = new Weather(R.drawable.mangga, "mangga");
+        data.add(mangga);
+
+        Weather manggis = new Weather(R.drawable.manggis, "manggis");
+        data.add(manggis);
+
+        Weather nanas = new Weather(R.drawable.nanas, "nanas");
+        data.add(nanas);
+
+        Weather pisang = new Weather(R.drawable.pisang, "pisang");
+        data.add(pisang);
+
+        Weather semangka = new Weather(R.drawable.semangka, "semangka");
+        data.add(semangka);
+
+        Weather pepaya = new Weather(R.drawable.pepaya, "pepaya");
+        data.add(pepaya);
+
+        Weather apel = new Weather(R.drawable.apel, "apel");
+        data.add(apel);
+        // ---
 
         final WeatherAdapter adapter = new WeatherAdapter(this, R.layout.layout_listitem, data);
         lvList.setAdapter(adapter);
@@ -47,9 +81,14 @@ public class ActivityWeather extends Activity {
         lvList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), data.get(position).title + " removed", Toast.LENGTH_SHORT).show();
-                data.remove(position);
-                adapter.notifyDataSetChanged();
+//                Toast.makeText(getApplicationContext(), data.get(position).title + " removed", Toast.LENGTH_SHORT).show();
+//                data.remove(position);
+//                adapter.notifyDataSetChanged();
+
+                Intent toTarget = new Intent(getApplicationContext(), ActivityTarget.class);
+                toTarget.putExtra("icon", data.get(position).icon);
+                toTarget.putExtra("title", data.get(position).title);
+                startActivity(toTarget);
             }
         });
     }
